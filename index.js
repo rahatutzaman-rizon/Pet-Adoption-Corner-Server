c57decff751ed66a7b7c978458862e237aaba30b
redwan-525 <redwantamim525@gmail.com>
2023-11-29 02:46:54 +0600

    add collection campaign

--- index.js
+++ index.js
@@ -34,12 +34,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+     await client.connect();
 
     ///create a collection of documents
     const petCollection = client.db('petcollection').collection('adopted');
-    const donateCollection = client.db('donatecollection').collection('donate');
-    const adoptCollection=client.db('adoptcollection').collection('adopt');
+    const donateCollection = client.db('petcollection').collection('campaign');
+    const adoptCollection=client.db('petcollection').collection('adopt');
+    const addCollection=client.db('petcollection').collection("add-pet");
+    const stipeCollection=client.db('petcollection').collection("stipe");
 ////insert a book post 
 //  app.post('/upload-book',async(req,res)=>{
 
@@ -51,11 +53,30 @@ async function run() {
 //   res.send(result);
 //  })
 
- ////insert a book post 
+//insert donation campign field 
+app.post('/donation-campaign', async(req,res)=>{
+  const newcart = req.body;
+
+ const result = await donateCollection.insertOne(newcart);
+
+ res.send(result);
+})
+
+app.get("/donation-campaign", async (req, res) => {
+const cursor = donateCollection.find();
+const result = await cursor.toArray();
+res.send(result);
+}); 
+
+
+
+
+ ////insert a pet post 
  app.post('/adopt', async(req,res)=>{
   const newcart = req.body;
- 
+ console.log(newcart)
  const result = await adoptCollection.insertOne(newcart);
+ console.log(result)
  res.send(result);
 })
 
@@ -63,7 +84,36 @@ app.get("/adopt", async (req, res) => {
 const cursor = adoptCollection.find();
 const result = await cursor.toArray();
 res.send(result);
-});
+}); 
+
+//add pet 
+app.post('/add-pet',async(req,res)=>{
+
+    const data=req.body;
+  
+    //
+    const result=await addCollection.insertOne(data);
+    
+    res.send(result);
+})
+
+app.get("/add-pet", async (req, res) => {
+  const cursor = addCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+  }); 
+
+ 
+
+    app.get("/add-pet/:id", async (req, res) => {
+      const id=req.params.id;
+     const query={
+      _id : new ObjectId(id)
+     }
+      const result = await addCollection.findOne(query) ;
+      res.send(result);
+    });
+
 
  ///MOREdetail a book
 
@@ -76,6 +126,31 @@ res.send(result);
   res.send(result);
 });
 
+//part2 stripe
+app.get("/moredetail2/:id", async (req, res) => {
+  const id=req.params.id;
+ const query={
+  _id : new ObjectId(id)
+ }
+  const result = await petCollection.findOne(query) ;
+  res.send(result);
+});
+
+//post it donation detail
+
+app.post('/donation-detail', async(req,res)=>{
+  const newcart = req.body;
+ console.log(newcart)
+ const result = await stipeCollection.insertOne(newcart);
+ console.log(result)
+ res.send(result);
+})
+
+app.get("/donation-detail", async (req, res) => {
+const cursor = stipeCollection.find();
+const result = await cursor.toArray();
+res.send(result);
+}); 
 
  //find to get
 
@@ -101,6 +176,27 @@ app.get('/pet-category',async(req,res)=>{
 
   res.send(result);
  })
+
+
+ ///update add-pet 
+ app.patch("/update-pet/:id", async(req,res)=>{
+
+  const id=req.params.id;
+const updateBooksData=req.body;
+const filter ={ _id :new ObjectId(id)};
+
+
+const updateDoc = {
+  $set: updateBooksData
+};
+const options = { upsert: true };
+
+const result=await addCollection.updateOne(filter,updateDoc,options);
+
+  res.send(result)
+ })
+    
+
 
 
  ///update a books
